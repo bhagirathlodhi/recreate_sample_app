b4529bae8ae85def20f11a1976f9f9bfcf371955
bhagirathlodhi <bhagi.lodhi8989@gmail.com>
2023-09-13 16:11:11 +0530

    Initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,11 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+// 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
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+import "bootstrap"
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  #get 'sessions/new'
+  
+  resources :users
+  root "static_pages#home"
+  get '/hello', to: "static_pages#hello"
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
